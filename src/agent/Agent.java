a519bb82f868c0f31f2e2cb1a302af2f5c8b5e5b
smcguire56 <sean.mcguire56@gmail.com>
2019-02-19 09:14:14 +0000

    Cleaned code, Q table printing

--- src/agent/Agent.java
+++ src/agent/Agent.java
@@ -5,26 +5,25 @@ public class Agent {
 	 * Initialise QTable: 
 	 * [obs1] = Cart Position 
 	 * [obs2] = Cart Velocity 
-	 * [obs3] =Pole Angle 
+	 * [obs3] = Pole Angle 
 	 * [obs4] = Pole Velocity At Tip
 	 * 
 	 * [act] = action 1 or 0
 	 * 
 	 */
 
-	private static int cartPosition = 49;
-	private static int cartVelocity = 21;
-	private static int poleAngle = 85;
-	private static int poleVelocity = 21;
-	private static int actions = 2;
+	private static int cartPosition = 1;
+	private static int cartVelocity = 1;
+	private static int poleAngle = 6;
+	private static int poleVelocity = 12;
 
-	private float alpha = 0.10f;
+	private float alpha = 0.1f;
 	private float gamma = 1.0f;
-	private float epsilon = 0.10f;
-	private int numActions;
-	
-	float[][][][][] qTable = new float[cartPosition][cartVelocity][poleAngle][poleVelocity][actions];
+	private float epsilon = 0.1f;
 
+	private int numActions = 2;
+
+	float[][][][][] qTable = new float[cartPosition][cartVelocity][poleAngle][poleVelocity][numActions];
 
 	public Agent(int numActions) {
 		this.numActions = numActions;
@@ -41,46 +40,35 @@ public class Agent {
 
 	private void initialiseQvalues() {
 
-		
 		System.out.print("Setting Q table to 0 ");
 
 		for (int i = 0; i < cartPosition; i++) {
 			for (int j = 0; j < cartVelocity; j++) {
 				for (int x = 0; x < poleAngle; x++) {
 					for (int y = 0; y < poleVelocity; y++) {
-						for (int z = 0; z < actions; z++) {
+						for (int z = 0; z < numActions; z++) {
 							qTable[i][j][x][y][z] = 0;
 						}
 					}
 				}
 			}
 		}
-		
-		/*System.out.println("Printing Q Table: ");
-		for (int i = 0; i < cartPosition; i++) {
-			for (int j = 0; j < cartVelocity; j++) {
-				for (int x = 0; x < poleAngle; x++) {
-					for (int y = 0; y < poleVelocity; y++) {
-						for (int z = 0; z < actions; z++) {
-							System.out.print(" "+qTable[i][j][x][y][z]);
-						}
-					}
-					System.out.println();
-				}
-				System.out.println();
-			}
-			System.out.println();
-		}*/
+
+		/*
+		 * System.out.println("Printing Q Table: "); for (int i = 0; i < cartPosition;
+		 * i++) { for (int j = 0; j < cartVelocity; j++) { for (int x = 0; x <
+		 * poleAngle; x++) { for (int y = 0; y < poleVelocity; y++) { for (int z = 0; z
+		 * < actions; z++) { System.out.print(" "+qTable[i][j][x][y][z]); } }
+		 * System.out.println(); } System.out.println(); } System.out.println(); }
+		 */
 	}
-	
-	// 
+
+	// update the Q value
 	public void updateQValue(int[] previousState, int selectedAction, int[] currentState, float reward) {
-		// implementation of Q-learning TD update rule (see slides #26, #27 )
+		// implementation of Q-learning TD update rule
 		float oldQ = qTable[previousState[0]][previousState[1]][previousState[2]][previousState[3]][selectedAction];
 		float maxQ = getMaxQValue(currentState);
 		float newQ = oldQ + alpha * (reward + gamma * maxQ - oldQ);
-		// 0,0 is 0, 0 1 is 1 and so on.
-		// action is 1 2 3 4 direction
 		qTable[previousState[0]][previousState[1]][previousState[2]][previousState[3]][selectedAction] = newQ;
 	}
 
@@ -91,42 +79,56 @@ public class Agent {
 	}
 
 	private int getMaxValuedAction(int[] state) {
-			// greedy action selection implementation
-			// return the index of the most valuable action for a particular state
-			int maxIndex = -1;
-			float maxValue = -Float.MAX_VALUE;
-			for (int action = 0; action < numActions; action++) {
-				if (qTable[state[0]][state[1]][state[2]][state[3]][action] > maxValue) {
-					maxIndex = action;
-					maxValue = qTable[state[0]][state[1]][state[2]][state[3]][action];
-				}
+		// greedy action selection implementation
+		// return the index of the most valuable action for a particular state
+		int maxIndex = -1;
+		float maxValue = -Float.MAX_VALUE;
+		for (int action = 0; action < numActions; action++) {
+			if (qTable[state[0]][state[1]][state[2]][state[3]][action] > maxValue) {
+				maxIndex = action;
+				maxValue = qTable[state[0]][state[1]][state[2]][state[3]][action];
 			}
-			return maxIndex;
 		}
+		return maxIndex;
+	}
 
 	public int selectAction(int[] state) {
-		// epsilon-greedy action selection strategy implementation (see slide #23)
+		// epsilon-greedy action selection strategy implementation
 		int selectedAction = -1;
 		double randomValue = Math.random();
 
-		//System.out.println("Agent: selecting action, epsilon=" + epsilon + ", randomValue=" + randomValue);
-	
-
-		if (randomValue < epsilon) { // select a random action with probability epsilon
+		// select a random action with probability epsilon
+		// else select the most valuable action
+		if (randomValue < epsilon) {
 			selectedAction = selectRandomAction();
-			//System.out.println("Agent: selected action " + selectedAction + " at random");
-			
-		} else { // else select the most valuable action
-			selectedAction = getMaxValuedAction(state);
-			//System.out.println("Agent: selected action " + selectedAction + " greedily");
-			
 		}
-		return selectedAction;	
+		else {
+			selectedAction = getMaxValuedAction(state);
+		}
+
+		return selectedAction;
 	}
-	
+
 	public int selectRandomAction() {
 		// select a random action between 0 and (numActions-1)
 		return (int) (Math.random() * numActions);
+	}
+
+	// getters and setters
+	public int getCartPosition() {
+		return cartPosition;
+	}
+
+	public int getCartVelocity() {
+		return cartVelocity;
+	}
+
+	public int getPoleAngle() {
+		return poleAngle;
+	}
+
+	public int getPoleVelocity() {
+		return poleVelocity;
 	}
 
 }
--- src/agent/Environment.java
+++ src/agent/Environment.java
@@ -4,28 +4,17 @@ import javaclient.GymJavaHttpClient;
 import javaclient.StepObject;
 
 public class Environment {
-	private int numEpisodes = 50;
-
-	private static int cartPosition = 49;
-	private static int cartVelocity = 21;
-	private static int poleAngle = 85;
-	private static int poleVelocity = 21;
+	private int numEpisodes = 10000;
 
 	private Agent agent;
-	private int[] currentAgentState = new int[] { 1, 1, 1, 1 }; // agent's current x,y position (state vector)
-	private int[] previousAgentState = new int[] { -1, -1, -1, -1 };
+	private int[] currentAgentState = new int[] { 0, 0, 0, 0 }; // agent's current state of agent
+	// private int[] previousAgentState = new int[] { -1, -1, -1, -1 };
+
 	private int numActions = 2;
 
 	private float alpha = 0.10f;
 	private float gamma = 1.0f;
 	private float epsilon = 0.10f;
-
-	float[][][][] array = new float[cartPosition][cartVelocity][poleAngle][poleVelocity];
-
-	int numBucketsObs0 = 2200; // CartPosition (-2.4 to 2.4) = -24 + 24 + 1 = 49 different buckets
-	int numBucketsObs1 = 210; // CartVelocity (-inf to inf) = -10 to 10 + 1 = 21
-	int numBucketsObs2 = 8000; // PoleAngle (-41.8 to 41.8) = -42 + 42 = 84 + 1 = 85
-	int numBucketsObs3 = 210; // PoleVelocity (-inf to inf) = -10 to 10 + 1 = 21
 
 	// obsValue is the initial observed value
 	// minValue is the minimum that observation can be
@@ -41,27 +30,24 @@ public class Environment {
 		}
 
 		float bucketIncrement = maxValue / numBuckets;
-		
+
 		float bucket = obsValue / bucketIncrement;
-		
+
 		bucketIndex = (int) bucket;
-/*		System.out.println("obsValue: "+obsValue);
-		System.out.println("minValue: "+minValue);
-		System.out.println("maxValue: "+maxValue);
-		System.out.println("numBuckets: "+numBuckets);
-		System.out.println("bucketIncrement: "+bucketIncrement);
-		System.out.println("bucket: "+bucket);
-		System.out.println("bucketIndex: "+bucketIndex);*/
-		
+
 		return bucketIndex;
 	}
 
 	public void runExperiments(String id, Object obs) {
 		setupAgent();
-
-		for (int i = 0; i < numEpisodes; i++) {
+		// reset agent position
+		currentAgentState[0] = agent.getCartPosition() / 2;
+		currentAgentState[1] = agent.getCartVelocity() / 2;
+		currentAgentState[2] = agent.getPoleAngle() / 2;
+		currentAgentState[3] = agent.getPoleVelocity() / 2;
+		for (int i = 1; i <= numEpisodes; i++) {
 			System.out.println("Episode: " + i);
-			doEpisode(i, id, obs);
+			doEpisode(id, obs);
 		}
 
 	}
@@ -71,9 +57,8 @@ public class Environment {
 
 	}
 
-	private void doEpisode(int currentEpisode, String id, Object obs) {
+	private void doEpisode(String id, Object obs) {
 		int action;
-		int counter = 0;
 		int max_steps = 2000;
 		float reward = 0;
 		boolean done = false;
@@ -85,56 +70,48 @@ public class Environment {
 		}
 
 		for (int j = 0; j < max_steps; j++) {
+
+			int[] currentAgentStateNo = currentAgentState;
+			
 			// get next action based on q table
-			// action = agent.selectAction(currentAgentState);
-			action = agent.selectRandomAction();
+			action = agent.selectAction(currentAgentStateNo);
+			// action = agent.selectRandomAction();
+
+			// previousAgentState = currentAgentState;
 
 			StepObject step;
 			try {
-				step = GymJavaHttpClient.stepEnv(id, action, true, false);
+				step = GymJavaHttpClient.stepEnv(id, action, true, true);
 				obs = step.observation;
 				done = step.done;
 				reward = step.reward;
-				counter++;
 			} catch (Exception e) {
 				System.out.println("EOF");
 				break;
 			}
+			// currentAgentState set to next state
+			String ObsToString = obs.toString();
+			ObsToString = ObsToString.replaceAll("\\[", "").replaceAll("\\]", "");
+			String[] observations = ObsToString.split(",");
+			currentAgentState[0] = getBucketIndex(Float.parseFloat(observations[0]), (float) -24, (float) 24,
+					(float) agent.getCartPosition());
+			currentAgentState[1] = getBucketIndex(Float.parseFloat(observations[1]), (float) -10, (float) 10,
+					(float) agent.getCartVelocity());
+			currentAgentState[2] = getBucketIndex(Float.parseFloat(observations[2]), (float) -42, (float) 42,
+					(float) agent.getPoleAngle());
+			currentAgentState[3] = getBucketIndex(Float.parseFloat(observations[3]), (float) -10, (float) 10,
+					(float) agent.getPoleVelocity());
 
-			String stringValues = obs.toString();
-			stringValues = stringValues.replaceAll("\\[", "").replaceAll("\\]", "");
-			String[] integerStrings = stringValues.split(",");
-
-			//System.out.println(integerStrings[0]+" "+ integerStrings[1]+" "+ integerStrings[2]+" "+ integerStrings[3]);
-
-			currentAgentState[0]  = getBucketIndex(Float.parseFloat(integerStrings[0]), (float)-24 , (float)24, (float)numBucketsObs0);
-			currentAgentState[1]  = getBucketIndex(Float.parseFloat(integerStrings[1]), (float)-10 , (float)10, (float)numBucketsObs1);
-			currentAgentState[2]  = getBucketIndex(Float.parseFloat(integerStrings[2]), (float)-42 , (float)42, (float)numBucketsObs2);
-			currentAgentState[3] = getBucketIndex(Float.parseFloat(integerStrings[3]), (float)-10 , (float)10, (float)numBucketsObs3);
-			
-			System.out.println(currentAgentState[0] + " " + currentAgentState[1] + " " + currentAgentState[2] + " " + currentAgentState[3]);
-			
-			currentAgentState[0] = 1;
 			// update q value table here
-			/*
-			 * System.out.println("Action: " + action); System.out.println("obs: " +
-			 * obs.toString()); System.out.println("done: " + done);
-			 * System.out.println("counter: " + counter); System.out.println("reward: " +
-			 * reward); System.out.println(GymJavaHttpClient.listEnvs());
-			 */
+			agent.updateQValue(currentAgentState, action, currentAgentState, reward);
+
+			System.out.println(currentAgentState[0] + " " + currentAgentState[1] + " " + currentAgentState[2] + " "
+					+ currentAgentState[3]);
 
 			if (done) {
 				break;
 			}
 		}
 	}
-
-	/*
-	 * private int obsToAction(Object obs) { JSONObject obj = new JSONObject();
-	 * Iterator<Object> iter = ((JSONArray) obs).iterator(); double sum = 0; while
-	 * (iter.hasNext()) { sum += (double) iter.next(); }
-	 * 
-	 * if (sum > 0) { return 1; } else { return 0; } }
-	 */
 
 }
